8c6a033c6f296886aa6c2459c1bcab2313ffeba3
Ssssvhhhhh <fracefrace1@gmail.com>
2026-02-24 19:13:19 +0300

    Authorization/Chat/Reg - ui

--- SimpleChat/main.cpp
+++ SimpleChat/main.cpp
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
     //Server server2(nullptr, 1235);
 
     MainWindow w;
-    MainWindow w2;
+    //MainWindow w2;
     //MainWindow w3;
 
     w.show();
-    w2.show();
+    //w2.show();
     //w3.show();
     return a.exec();
 }
